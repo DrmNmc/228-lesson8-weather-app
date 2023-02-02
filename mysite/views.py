3a42ff68c83bd91d9e195afa905521575471e0b3
Dylan <dylan.r.miller517@gmail.com>
2023-02-02 02:45:15 -0500

    nearly complete

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import os
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -10,39 +11,53 @@ def home(request):
     form = LocationForm()
     return render(request, 'index.html', {'form': form})
 
+def get_weekdays(dates):
+    weekdays = []
+    for date in dates:
+        weekdays.append(datetime.strftime(date, '%A'))
+    return weekdays
+
+def get_dates_list(num_days):
+    today = datetime.today().date()
+    dates = [today + timedelta(days=x) for x in range(num_days)]
+    return dates
+
 
 def get_forecast(weather_data):
-    forecast = weather_data['forecast']['forecastDay']['date']
     forecastDict = {}
-    for i in range(5):
-        date = forecast[i]['date']
-        minTemp = forecast[i]['day']['mintemp_f']
-        maxTemp = forecast[i]['day']['maxtemp_f']
-        forecastDict[f'date{i}'] = date
-        forecastDict[f'minTemp{i}'] = minTemp
-        forecastDict[f'maxTemp{i}'] = maxTemp
+    dates = get_dates_list(5)
+    weekdays = get_weekdays(dates)
+
+    for i, date in enumerate(dates):
+        forecast = weather_data['forecast']['forecastday'][i]
+        dateMatch = forecast['date']
+        if dateMatch == str(date):
+            wkday = weekdays[i]
+            minTemp = forecast['day']['mintemp_f']
+            maxTemp = forecast['day']['maxtemp_f']
+            forecastDict['date'] = dateMatch
+            forecastDict[dateMatch] = [wkday, minTemp, maxTemp]
     return forecastDict
 
 
 def get_weather(request):
     token = os.environ.get('token')
     ip_address = requests.get('https://api.ipify.org').text
-    #if ip_address:
-    url = f"http://api.weatherapi.com/v1/forecast.json?key={token}&q={ip_address}&aqi=no&days=5"
-    response = requests.get(url)
-    weather_data = response.json()
-    # else:
-    #     city= request.POST['city']
-    #     state_code=request.POST['state']
-    #     url = f"http://api.weatherapi.com/v1/current.json?key={token}&q={city}, {state_code}&aqi=no&days=5"
-    #     #city, country_code?
-    #     response = requests.get(url)
-    #     weather_data = response.json()
+    if request.method == "POST":
+        city = request.POST.get('city')
+        state_code = request.POST.get('state')
+        url = f"http://api.weatherapi.com/v1/forecast.json?key={token}&q={city},{state_code}&aqi=no&days=5"
+        response = requests.get(url)
+        weather_data = response.json()
+    else:
+        url = f"http://api.weatherapi.com/v1/forecast.json?key={token}&q={ip_address}&aqi=no&days=5"
+        response = requests.get(url)
+        weather_data = response.json()
     temperature = weather_data['current']['temp_f']
     forecastDict = get_forecast(weather_data)
-    s = "The temp is {} degrees.".format(
+    s = "The temp is {}°F.".format(
          temperature)
-    data = {"weather_data": s, "forecastDict": forecastDict}
+    data = {"forecastDict": forecastDict}
     return JsonResponse(data)
 
 
