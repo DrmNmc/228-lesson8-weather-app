14227310c6fc8b3c52ab6b9e6a181919a458847f
Dylan <dylan.r.miller517@gmail.com>
2023-02-01 19:45:37 -0500

    misc save

--- mysite/views.py
+++ mysite/views.py
@@ -12,25 +12,23 @@ def home(request):
 
 
 def get_forecast(weather_data):
-    forecast = weather_data['forecast']['forecastday']
+    forecast = weather_data['forecast']['forecastDay']['date']
     forecastDict = {}
     for i in range(5):
         date = forecast[i]['date']
         minTemp = forecast[i]['day']['mintemp_f']
         maxTemp = forecast[i]['day']['maxtemp_f']
-        totalSnow = forecast[i]['day']['totalsnow_cm']
         forecastDict[f'date{i}'] = date
         forecastDict[f'minTemp{i}'] = minTemp
         forecastDict[f'maxTemp{i}'] = maxTemp
-        forecastDict[f'totalSnow{i}'] = totalSnow
     return forecastDict
 
 
 def get_weather(request):
     token = os.environ.get('token')
-    ip_address = request.GET.get("ip")
+    ip_address = requests.get('https://api.ipify.org').text
     #if ip_address:
-    url = f"http://api.weatherapi.com/v1/current.json?key={token}&q={ip_address}&aqi=no&days=5"
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={token}&q={ip_address}&aqi=no&days=5"
     response = requests.get(url)
     weather_data = response.json()
     # else:
@@ -40,13 +38,11 @@ def get_weather(request):
     #     #city, country_code?
     #     response = requests.get(url)
     #     weather_data = response.json()
-    description = weather_data['current']['condition']['text']
     temperature = weather_data['current']['temp_f']
     forecastDict = get_forecast(weather_data)
-    # s = "You're in {}, {}. You can expect {} conditions with a temperature of {} degrees.".format(
-    #     city, state_code, description, temperature)
-    s = weather_data
-    data = {"weather_data": s, "forecast": forecastDict}
+    s = "The temp is {} degrees.".format(
+         temperature)
+    data = {"weather_data": s, "forecastDict": forecastDict}
     return JsonResponse(data)
 
 
